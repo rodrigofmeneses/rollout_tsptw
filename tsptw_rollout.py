c5a3faf79e462367939a6c812646ee997670f027
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2021-07-13 16:23:26 -0300

    backtracking algorithm

--- tsptw_rollout.py
+++ tsptw_rollout.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.arrayprint import set_printoptions
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 
@@ -52,11 +53,24 @@ def read_data(path):
             coordinates.pop()
             intervals.pop()
 
+    dimension = len(coordinates)
+
+    # dist_matrix = np.zeros((dimension, dimension))
+
+    # for i in range(dimension):
+    #     for j in range(i + 1, dimension):
+    #         px = np.power(coordinates[i][0] - coordinates[j][0], 2)
+    #         py = np.power(coordinates[i][1] - coordinates[j][1], 2)
+    #         dist = np.floor(np.sqrt(px + py))
+    #         dist_matrix[i, j] = dist
+    #         dist_matrix[j, i] = dist
+
     problem = {
         'coordinates': coordinates,
         'intervals': intervals,
+        # 'distance_matrix': dist_matrix,
         'distance_matrix': np.floor(squareform(pdist(coordinates))),
-        'dimension': len(coordinates)
+        'dimension': dimension
     }
     
     return problem
@@ -99,11 +113,61 @@ def is_feasible(solution, distance_matrix, intervals):
     
     return True
 
-
-def random_policy(solution, distance_matrix, intervals):
+def one_step_viability(solution, current_time, distance_matrix, intervals):
     
-    pass
+    all_states = list(range(distance_matrix.shape[0]))
+    actions = []
 
+    for step in all_states:
+        if step in solution:
+            continue
+        one_step_time = current_time + distance_matrix[solution[-1], step]
+        # if one_step_time >= intervals[step][0] \
+        #     and one_step_time <= intervals[step][1]:
+        if one_step_time <= intervals[step][1]:
+            actions.append(step)
+    
+    return actions
+
+
+
+def random_policy(solution, current_time, distance_matrix, intervals):
+    
+    num_nodes = distance_matrix.shape[0]
+    level = []
+    time_control = [current_time]
+    i = 0
+    
+    while len(solution) != num_nodes:
+        # Existem ações pra seguir?
+        if i >= len(level):
+            actions = one_step_viability(solution, current_time, distance_matrix, intervals)
+            # if actions == []:
+            #     # Infeasible
+            #     return 9999
+            level.append(actions.copy())
+            continue
+
+        if level[i] != []:
+            step = np.random.choice(level[i])
+            level[i].remove(step)
+            solution.append(step)
+            if current_time + distance_matrix[solution[-2], solution[-1]]\
+                <= intervals[solution[-1]][0]:
+                current_time = intervals[solution[-1]][0]
+            else:
+                current_time += distance_matrix[solution[-2], solution[-1]]
+            
+            time_control.append(current_time)
+            i += 1
+        else:
+            level.pop()
+            solution.pop()
+            i -= 1
+            current_time = time_control[i]
+            time_control.pop()
+
+    return solution
 
 def rollout_algorithm(problem, starting_node=0):
     # Distance Matrix
@@ -146,14 +210,16 @@ def rollout_algorithm(problem, starting_node=0):
 
     return solution
 
-
-path = 'instances/tsptw_data/DumasEtAl/n20w20.001.txt'
+np.random.seed(13123123)
+# path = '/home/rodrigomeneses/Documents/repositorios/rollout_tsptw/instances/tsptw_data/DumasEtAl/n20w20.002.txt'
+path = '/home/rodrigomeneses/Documents/repositorios/rollout_tsptw/instances/tsptw_data/DumasEtAl/n40w20.001.txt'
 problem = read_data(path)
 
-solution = np.array([1, 17, 10, 20, 18, 19, 11, 6, 16, 2, 12, 13, 7, 14, 8, 3, 5, 9, 21, 4, 15, 1]) - 1
-# solution = np.array([1, 2, 15, 19, 5, 9, 8, 6, 16, 12, 17, 20, 18, 11, 13, 4, 7, 3, 21, 10, 14, 1]) - 1
+# solution = np.array([1, 17, 10, 20, 18, 19, 11, 6, 16, 2, 12, 13, 7, 14, 8, 3, 5, 9, 21, 4, 15, 1]) - 1
+solution = np.array([1, 2, 15, 19, 5, 9, 8, 6, 16, 12, 17, 20, 18, 11, 13, 4, 7, 3, 21, 10, 14, 1]) - 1
 # solution = np.array([1, 6, 20, 14, 2, 9, 17, 5, 10, 4, 8, 16, 15, 18, 12, 3, 11, 19, 21, 7, 13, 1]) - 1
 # solution = np.array([1, 12, 4, 3, 20, 8, 16, 10, 9, 6, 7, 11, 15, 5, 13, 17, 19, 14, 21, 18, 2, 1]) - 1
 # solution = np.array([1, 20, 12, 8, 19, 17, 14, 9, 4, 18, 3, 6, 11, 5, 16, 10, 15, 7, 21, 2, 13, 1]) - 1
 print(calculate_solution_cost(solution, problem['distance_matrix'], problem['intervals']))
 print(is_feasible(solution, problem['distance_matrix'], problem['intervals']))
+print(random_policy([0], 0, problem['distance_matrix'], problem['intervals']))
